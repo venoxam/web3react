42bc85c538837ff5a9dfe0d9e940662079505af6
Maxence Van Bockstaele <maxence.vanbockstaele@student.vinci.be>
2023-09-22 12:39:27 +0200

    ex2

---
+++ theorie/Module 2/module2theorie/src/components/App/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import Display from '../Display/Display'
+import Button from '../Button/Button'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter} />
+
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
+  )
+}
+
+export default App;
---
+++ theorie/Module 2/module2theorie/src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './components/App/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
